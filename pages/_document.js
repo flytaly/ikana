4e4aaf97bed6bb91b1b594d48ea61b3d855a059e
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-07-09 15:46:16 +0300

    Add lang attribute to html

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 // injecting the server side rendered styles into the <head>
 // https://github.com/zeit/next.js/tree/master/examples/with-styled-components
 
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -27,5 +27,17 @@ export default class MyDocument extends Document {
         } finally {
             sheet.seal();
         }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
